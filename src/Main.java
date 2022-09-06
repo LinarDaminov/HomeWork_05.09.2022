460bf58cd36eb047459f5431f8ce87092cb57fce
LinarDaminov <linar.daminov@yandex.ru>
2022-09-06 21:09:18 +0500

    Незаконченное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,8 @@ public class Main {
 
         // Задача 4
         System.out.println("Задача 4");
-        for (var w = 10; w > -10; w--);{
-            System.out.println();
+        for(int i = 10; i > -10; i--) {
+            System.out.println(i);
         }
 
         //  Задача 5
@@ -49,6 +49,16 @@ public class Main {
         for (int i = 0; i <= 12; i++) {
             total = total+salary;
             System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+
+        // Задача 9
+        System.out.println("Задача 9");
+        int salaryMonth = 29000;
+        int totalYear = 0;
+        for (int i = 0; i <= 12; i++) {
+            totalYear = totalYear+ totalYear/100;
+            totalYear = totalYear+salaryMonth;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + totalYear + " рублей");
         }
         }
 
